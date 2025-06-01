3a6afbed96f3c836d9bddbc33ffb2bc59649b187
koscrob <koscrob@users.noreply.github.com>
2025-06-01 02:01:55 +0200

    Somewhat fixed the blending of tiles when using tiled VAE render.

--- ggml_extend.hpp
+++ ggml_extend.hpp
@@ -488,9 +488,11 @@ __STATIC_INLINE__ void ggml_merge_tensor_2d(struct ggml_tensor* input,
                     const float x_f = std::min(std::min(x_f_0, x_f_1), 1.f);
                     const float y_f = std::min(std::min(y_f_0, y_f_1), 1.f);
 
+                    float step = ggml_smootherstep_f32(y_f) * ggml_smootherstep_f32(x_f);
+
                     ggml_tensor_set_f32(
                         output,
-                        old_value + new_value * ggml_smootherstep_f32(y_f) * ggml_smootherstep_f32(x_f),
+                        old_value * (1.f - step) + new_value * step,
                         x + ix, y + iy, k);
                 } else {
                     ggml_tensor_set_f32(output, new_value, x + ix, y + iy, k);
