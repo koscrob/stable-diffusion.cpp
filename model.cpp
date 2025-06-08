e3a0364facbaae85d722ba195305a8df07200c5a
koscrob <koscrob@users.noreply.github.com>
2025-06-09 00:13:11 +0200

    Fix to the previous commit.

--- model.cpp
+++ model.cpp
@@ -1818,7 +1818,7 @@ bool ModelLoader::load_tensors(on_new_tensor_cb_t on_new_tensor_cb, ggml_backend
             }
 
             // TODO: Only convert float16 to bf16 if the CPU does not support half->float conversion.
-            if (dst_tensor->buffer == NULL && tensor_storage.type == GGML_TYPE_F16) {
+            if (ggml_backend_buffer_is_host(dst_tensor->buffer) && tensor_storage.type == GGML_TYPE_F16) {
                 dst_tensor->type = GGML_TYPE_BF16;
             }
 
